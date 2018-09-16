55140def43f9d0594b8d3fef2469e02d10ec3bf5
paulo-neto <pasvsi@gmai.com>
2018-09-16 14:24:31 -0300

    melhorando o tratamento de erros

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -64,6 +64,9 @@ export class NegociacaoController {
                     this.negociacoes.adiciona(negociacao));
 
                 this.negociacoesView.update(this.negociacoes);
+            })
+            .catch(err => {
+                this.mensagemView.update(err.message);
             });
     }
 
--- app/ts/models/index.ts
+++ app/ts/models/index.ts
@@ -2,3 +2,4 @@ export {Negociacao} from './Negociacao';
 export {Negociacoes} from './Negociacoes';
 export {NegociacaoParcial} from './NegociacaoParcial';
 export {MeuObjeto} from './MeuObjeto';
+export {Imprimivel} from './Imprimivel';
