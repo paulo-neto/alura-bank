8980092934c00b130a124eaec28d0e7c678c1d55
paulo-neto <pasvsi@gmai.com>
2018-09-16 13:58:09 -0300

    add interface MeuObjeto (Object)

--- app/ts/models/index.ts
+++ app/ts/models/index.ts
@@ -1,4 +1,4 @@
 export {Negociacao} from './Negociacao';
 export {Negociacoes} from './Negociacoes';
 export {NegociacaoParcial} from './NegociacaoParcial';
-export {Imprimivel} from './Imprimivel';
+export {MeuObjeto} from './MeuObjeto';
